7f6f8d206634d456e3f17d71b9565fb3a894a49c
26Ammu <amruthark2003@gmail.com>
2026-03-29 21:52:23 +0530

    Add files via upload

---
+++ credo/subpack7.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pack7;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author amrut
+ */
+public class subpack7 {
+     public Connection getConnection() {
+        try {
+            // Dynamically load the MySQL JDBC driver class
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            // Attempt to establish a connection to the MySQL database
+            // URL: jdbc:mysql://localhost:3306/parkinglot
+            // Username: root
+            // Password: (empty)
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/credo", "root", "");
+            
+            // Return the established connection
+            return con;
+        } catch (ClassNotFoundException | SQLException e) {
+            // Print the exception details to the console if an error occurs
+            System.out.println(e);
+        }
+        
+        // Return null if the connection could not be established
+        return null;
+     }
+}
