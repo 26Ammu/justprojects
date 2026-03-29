37f22ea4fbbc185f496a7702e61c079234d4038e
26Ammu <amruthark2003@gmail.com>
2026-03-29 21:56:08 +0530

    Add files via upload

---
+++ credo/add_bankdb.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pack7.subpack7;
+
+/**
+ *
+ * @author amrut
+ */
+@WebServlet(urlPatterns = {"/add_bankdb"})
+public class add_bankdb extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+            String branch_name=request.getParameter("branch_name");
+         String location=request.getParameter("location");
+          
+            
+              out.print(branch_name+" "+location);
+              
+              try{
+                 Class.forName("com.mysql.jdbc.Driver");
+                Connection con=new subpack7().getConnection();
+                Statement stat=con.createStatement();
+                stat.executeUpdate("INSERT INTO `branch`(`branch_name`, `location`) VALUES ('"+branch_name+"','"+location+"')");
+                
+                out.println("<script>alert(\"saved successfully \")</script>");
+            out.println("<meta http-equiv = \"refresh\" content = \"0;view_allbanks.jsp\" />"); 
+             }
+            catch(Exception e){
+                out.println(e);
+                
+            }
+        /* TODO output your page here. You may use following sample code. */
+           
+        }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
