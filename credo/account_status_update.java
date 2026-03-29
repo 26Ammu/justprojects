b65f32b0debd65a68a1b49c3220636fd7401f961
26Ammu <amruthark2003@gmail.com>
2026-03-29 21:55:30 +0530

    Add files via upload

---
+++ credo/account_status_update.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pack7.subpack7;
+
+/**
+ *
+ * @author amrut
+ */
+@WebServlet(urlPatterns = {"/account_status_update"})
+public class account_status_update extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+    String accountIdStr = request.getParameter("account_id");
+    String status = request.getParameter("status");
+    String customerId = request.getParameter("customer_id");
+
+    try {
+        int accountId = Integer.parseInt(accountIdStr);
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = new subpack7().getConnection();
+        Statement stat = con.createStatement();
+        
+        String query = "UPDATE account SET status='" + status + "' WHERE account_id=" + accountId;
+        stat.executeUpdate(query);
+
+        con.close();
+
+        
+       
+        response.sendRedirect("view_account.jsp?customer_id=" + customerId);
+
+    } catch (Exception e) {
+        response.getWriter().println("Error: " + e.getMessage());
+    }
+}
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
